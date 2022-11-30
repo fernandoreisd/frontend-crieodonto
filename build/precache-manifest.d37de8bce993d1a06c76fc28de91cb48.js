77dc438086b9b2ae906ee29c408d64087c37a444
fernandoreisd <fernando@fasterisk.com.br>
2022-11-30 14:37:03 -0300

    Frontend-Crieodonto

---
+++ build/precache-manifest.d37de8bce993d1a06c76fc28de91cb48.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3257f282859c8e36c71c219689f2460b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be7fd6eb0dffa155b23f",
+    "url": "/static/css/2.38dcfae8.chunk.css"
+  },
+  {
+    "revision": "be7fd6eb0dffa155b23f",
+    "url": "/static/js/2.917f6061.chunk.js"
+  },
+  {
+    "revision": "8176835c162b058bc9ca80d7d4403daf",
+    "url": "/static/js/2.917f6061.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "134e4a2ff4059251499c",
+    "url": "/static/js/3.dd216c34.chunk.js"
+  },
+  {
+    "revision": "c15eddeb09b46511cf0731116c7067b0",
+    "url": "/static/js/3.dd216c34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d1c8368cbb0a2d3c871",
+    "url": "/static/js/main.2cc97e63.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.2cc97e63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "722fce803d054f11d04b",
+    "url": "/static/js/runtime-main.0f1fdfb8.js"
+  },
+  {
+    "revision": "7d249758bfb3573f441d8dfd10f85e7c",
+    "url": "/static/media/logo.7d249758.png"
+  },
+  {
+    "revision": "8e65b9799948d1af69be247b167e073d",
+    "url": "/static/media/sound.8e65b979.mp3"
+  },
+  {
+    "revision": "adbccee0708ae3b7a71d9652fb353299",
+    "url": "/static/media/wa-background.adbccee0.png"
+  }
+]);
